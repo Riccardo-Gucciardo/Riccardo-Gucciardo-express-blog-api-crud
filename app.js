5c28d2a5dab4100bfd36d7853b442f560f48a0fe
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-18 15:07:33 +0100

    middleware not found

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express()
 const port = 3000;
 const postrouter = require("./routers/postsRouter")
+const notFound = require("./middleware/notFound")
 
 app.use(express.static('public'));
 app.use(express.json());
 
 
+
 app.use('/api/posts', postrouter)
 
-
+app.use(notFound);
 app.listen(port, ()=>{
     console.log(`example app listening on port ${port}`);  
      

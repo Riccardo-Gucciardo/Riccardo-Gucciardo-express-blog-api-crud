e48e2f03aae0cc712dfa8c8b6de34777ae92ee33
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-26 19:56:49 +0100

    aggiunta cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express()
+const cors = require ('cors')
 const port = 3000;
 const postrouter = require("./routers/postsRouter")
 const notFound = require("./middleware/notFound")
@@ -8,7 +9,9 @@ const errorHandler = require("./middleware/errorHandler")
 app.use(express.static('public'));
 app.use(express.json());
 
-
+app.use(cors({
+    origin : 'http://localhost:5173/'
+}))
 
 app.use('/api/posts', postrouter)
 
